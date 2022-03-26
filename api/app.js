d1f3c93d0bf69b017b16d1417ad371fbe09270a2
Denizhan Gonen <gonendenizhan@gmail.com>
2022-03-26 12:25:34 +0300

    initial node-express-mongo setup

---
+++ api/app.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const port = 8080;
+
+// const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set headers to prevent cors errors on client side
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, HEAD'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+
+  res.status(status).json({ message: message });
+});
+
+console.log('process.env.MONGO_USER : ', process.env.MONGO_USER);
+console.log('process.env.MONGO_USER_PASS : ', process.env.MONGO_USER_PASS);
+console.log('process.env.MONGO_DB : ', process.env.MONGO_DB);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_USER_PASS}@cluster0-8gblk.mongodb.net/${process.env.MONGO_DB}`
+  )
+  .then((result) => {
+    app.listen(process.env.PORT || port);
+  })
+  .catch((err) => {
+    console.log(
+      '----------------------------------Mongo Connection Error--------------------------------'
+    );
+    console.log('process.env.MONGO_USER : ', process.env.MONGO_USER);
+    console.log('process.env.MONGO_USER_PASS : ', process.env.MONGO_USER_PASS);
+    console.log('process.env.MONGO_USER_PASS : ', process.env.MONGO_DB);
+    console.log(err);
+  });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
